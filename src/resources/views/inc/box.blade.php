6434b8cf70c294cf29dfd780937de9d2b108c889
Shreya Pramanik <pramanikshreya@gmail.com>
2018-12-16 23:49:21 +0530

    The null coalescing operator (??) has been added as part of PHP 7 upgrade

--- src/resources/views/inc/box.blade.php
+++ src/resources/views/inc/box.blade.php
@@ -1,11 +1,11 @@
-<div class="card {{ $class_list or '' }}">
+<div class="card {{ $class_list ?? '' }}">
     @isset($title)
     <div class="card-header">
         <h4 class="card-title">{{ $title }}</h4>
         @isset($subtitle)<p class="card-category">{{ $subtitle }}</p>@endisset
     </div>
     @endisset
-    <div class="card-body {{ $body_class_list or '' }}">
+    <div class="card-body {{ $body_class_list ?? '' }}">
         {{ $body }}
     </div>
     @isset($footer)
